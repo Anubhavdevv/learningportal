14629b04d42e9ea07ab49f19c58c57cf1546219a
anubhavdevv <guptaanubhav907@gmail.com>
2022-07-02 04:10:48 +0530

    Initial setup for dashboard

---
+++ src/Data/Data.js
@@ -0,0 +1,93 @@
+// Sidebar imports
+import {
+  UilEstate,
+  UilClipboardAlt,
+  UilUsersAlt,
+  UilPackage,
+  UilChart,
+  UilSignOutAlt,
+} from "@iconscout/react-unicons";
+
+// Analytics Cards imports
+import { UilUsdSquare, UilMoneyWithdrawal } from "@iconscout/react-unicons";
+import { keyboard } from "@testing-library/user-event/dist/keyboard";
+
+// Recent Card Imports
+
+// Sidebar Data
+export const SidebarData = [
+  {
+    icon: UilEstate,
+    heading: "Dashboard",
+  },
+  {
+    icon: UilClipboardAlt,
+    heading: "Course",
+  },
+  {
+    icon: UilUsersAlt,
+    heading: "Student",
+  },
+  {
+    icon: UilPackage,
+    heading: 'Discussion'
+  },
+  {
+    icon: UilChart,
+    heading: 'Schedule'
+  },
+];
+
+// Analytics Cards Data
+export const cardsData = [
+  {
+    title: "LiveCourse",
+    color: {
+      backGround: "linear-gradient(180deg, #bb67ff 0%, #c484f3 100%)",
+      boxShadow: "0px 10px 20px 0px #e0c6f5",
+    },
+    barValue: 13,
+    value: "13",
+    png: UilUsdSquare,
+    series: [
+      {
+        name: "course",
+        data: [31, 40, 28, 51, 42, 109, 100],
+      },
+    ],
+  },
+  {
+    title: "videos",
+    color: {
+      backGround: "linear-gradient(180deg, #FF919D 0%, #FC929D 100%)",
+      boxShadow: "0px 10px 20px 0px #FDC0C7",
+    },
+    barValue: 276,
+    value: "276",
+    png: UilMoneyWithdrawal,
+    series: [
+      {
+        name: "Revenue",
+        data: [10, 100, 50, 70, 80, 30, 40],
+      },
+    ],
+  },
+  {
+    title: "students",
+    color: {
+      backGround:
+        "linear-gradient(rgb(248, 212, 154) -146.42%, rgb(255 202 113) -46.42%)",
+      boxShadow: "0px 10px 20px 0px #F9D59B",
+    },
+    barValue: 321,
+    value: "321",
+    png: UilClipboardAlt,
+    series: [
+      {
+        name: "Expenses",
+        data: [10, 25, 15, 30, 12, 15, 20],
+      },
+    ],
+  },
+];
+
